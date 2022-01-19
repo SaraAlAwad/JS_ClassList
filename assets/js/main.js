5af8ed8486302507455657af0d9fdf8d04bd192c
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-01-19 14:34:06 +0100

    2

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+
+function changeHeadline() {
+    document.getElementById("headline").classList.add("newStyle");
+}
+function restHeadline() {
+    document.getElementById("headline").classList.remove("newStyle");
+}
